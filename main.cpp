77d760a1f73b2c36aab4da41ec3c257d0d51ee0d
Захаров Артем <212-zaharov@lutch.ru>
2015-12-17 10:12:59 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,214 @@
+#include <iostream>
+#include <time.h>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <stdio.h>
+#include <random>
+#include <signal.h>
+#include <unistd.h>
+#include <atomic>
+
+#include <X11/Xlib.h>
+#include <sys/resource.h>
+#include <pthread.h>
+
+using namespace std;
+
+void _sleep(int64_t ms)
+{
+	timespec ts;
+	ts.tv_nsec = (ms * 1000000) % (int64_t)(1e+9);
+	ts.tv_sec = ms / 1000;
+	clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, 0);
+}
+
+const double mean = 0;
+const double sgm = 0.7;
+
+std::random_device rd;
+std::atomic<bool> done(false);
+
+namespace automatic_control{
+
+/**
+ * @brief The pidregulator struct
+ * calculation for PID automatic control
+ */
+struct pidregulator{
+	double current;
+	double speed;
+	double needed;
+	double prev_error;
+	double intg;		/// integreal value
+	double coeff0;		/// coefficient for summator
+	double coeff1;		/// coefficient for protoprtional part
+	double coeff2;		/// coefficient for integral part
+	double coeff3;		/// coefficient for diffirential part
+	int counter;
+	std::normal_distribution<double> distr;
+
+	pidregulator(){
+		init();
+	}
+	/**
+	 * @brief pidregulator
+	 * @param n
+	 * @param c0	- summator
+	 * @param c1	- proportional
+	 * @param c2	- integral
+	 * @param c3	- diffirential
+	 */
+	pidregulator(double n, double c0, double c1, double c2, double c3){
+		init();
+		needed = n;
+		coeff0 = c0;
+		coeff1 = c1;
+		coeff2 = c2;
+		coeff3 = c3;
+	}
+	void init(){
+		current		= 0;
+		needed		= 0;
+		intg		= 0;
+		speed		= 0;
+		counter		= 0;
+		prev_error	= 0;
+		coeff0 = coeff1 = coeff2 = coeff3 = 0;
+		distr = std::normal_distribution<double>(mean, sgm);
+	}
+	/**
+	 * @brief error
+	 * @return
+	 */
+	inline double error(){
+		return needed - current;
+	}
+	/**
+	 * @brief proportional
+	 * proportional part
+	 * @return
+	 */
+	inline double proportional(){
+		return coeff1 * error();
+	}
+	/**
+	 * @brief integral
+	 * integral part
+	 * @return
+	 */
+	inline double integral(){
+		intg += error();
+		return coeff2 * intg;
+	}
+	/**
+	 * @brief diff
+	 * differential part
+	 * @return
+	 */
+	inline double diff(){
+		double res =  coeff3 * (error() - prev_error);
+		prev_error = error();
+		return res;
+	}
+	/**
+	 * @brief calc
+	 * calc PID regulator
+	 */
+	void calc(){
+		double res1, res2, res3;
+		res1 = proportional();
+		res2 = integral();
+		res3 = diff();
+		speed += coeff0 * (res1 + res2 + res3);
+
+		char buf[512];
+		sprintf(buf, "%d\tc[%f]\tp[%f]\ti[%f]\td[%f]\tspd[%f]", counter, current,
+				res1, res2, res3, speed);
+		cout << buf << endl;
+		counter++;
+	}
+	/**
+	 * @brief new_current
+	 */
+	void new_current(){
+		double rnd = distr(rd);
+		speed += rnd;
+		current += speed;
+	}
+};
+
+}
+
+void loop_time(automatic_control::pidregulator& pid)
+{
+	pid.calc();
+	pid.new_current();
+}
+
+void handler(int s)
+{
+	done = true;
+	printf("app will close\n");
+	exit(1);
+}
+
+bool getkey_val(char* keys, int key)
+{
+	int ind1 = key / 8;
+	int ind2 = key % 8;
+	return (keys[ind1] & ind2) != 0;
+}
+
+void* pthread_run(void* user)
+{
+	char keys[32] = { 0 };
+
+	Display *display = 0;
+
+	display = XOpenDisplay(":0");
+
+	if(!display){
+		std::cout << "display not open\n";
+		return 0;
+	}
+
+	while(!done){
+		XQueryKeymap(display, keys);
+		if(getkey_val(keys, 10)){
+			done = true;
+		}
+
+		usleep(100 * 1000);
+	}
+
+	XCloseDisplay(display);
+}
+
+int main()
+{
+	/// hook ctrl+c
+	struct sigaction sa;
+	sa.sa_handler = handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+
+	sigaction(SIGINT, &sa, 0);
+
+	pthread_t ptr;
+	pthread_attr_t pattr;
+	std::fill((char*)&pattr, (char*)&pattr + sizeof(pattr), '\0');
+	/// loop to get a key
+	pthread_create(&ptr, &pattr, &pthread_run, 0);
+
+	automatic_control::pidregulator pid(100, 0.9, 0.19, 0.001, 0.95);
+	while(!done){
+		loop_time(pid);
+		_sleep(300);
+	}
+
+	pthread_join(ptr, 0);
+
+	return 0;
+}
+
